cd0d700a37c3d6971653d07a6d5e58a0176f0c60
ZoanHy <huyleezoan@gmail.com>
2025-08-11 09:32:19 +0700

    each child should have a unit key

--- src/components/todo/TodoData.jsx
+++ src/components/todo/TodoData.jsx
@@ -5,6 +5,7 @@ const TodoData = (props) => {
     <div className="todo-data">
       {todoList.map((item) => {
         return (
+          // need key to not render not using index
           <div key={item.id} className="todo-item">
             <div>{item.name}</div>
             <button>Delete</button>
