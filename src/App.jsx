226097c0558550072e39967389bbef55e53ba4e5
ZoanHy <huyleezoan@gmail.com>
2025-08-11 13:46:28 +0700

    nested routes với outlet

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import "./components/todo/todo.css";
 import { useState } from "react";
 import Header from "./components/layouts/header";
 import Footer from "./components/layouts/footer";
+import { Outlet } from "react-router-dom";
 
 function App() {
   const [todoList, setTodoList] = useState([]);
@@ -43,6 +44,8 @@ function App() {
           </div>
         )}
       </div>
+
+      <Outlet />
       <Footer />
     </>
   );
