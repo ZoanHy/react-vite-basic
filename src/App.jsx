9bf21dcbdb51bf84ba45aee1c43fd4ce4e8ebb90
ZoanHy <huyleezoan@gmail.com>
2025-08-11 14:02:11 +0700

    index route

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,11 @@
-import TodoNew from "./components/todo/TodoNew";
-import TodoData from "./components/todo/TodoData";
-import reactLogo from "./assets/react.svg";
-import "./components/todo/todo.css";
-import { useState } from "react";
 import Header from "./components/layouts/header";
 import Footer from "./components/layouts/footer";
 import { Outlet } from "react-router-dom";
 
 function App() {
-  const [todoList, setTodoList] = useState([]);
-
-  const addNewTodo = (name) => {
-    const newTodo = {
-      id: randomIntFromInterval(1, 1000000),
-      name: name,
-    };
-
-    setTodoList([...todoList, newTodo]);
-  };
-
-  const deleteATodo = (id) => {
-    const newTodoList = todoList.filter((item) => item.id !== id);
-    setTodoList(newTodoList);
-  };
-
-  const randomIntFromInterval = (min, max) => {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  };
-
   return (
     <>
       <Header />
-      <div className="todo-container">
-        <h1 className="todo-title">Todo List</h1>
-
-        <TodoNew addNewTodo={addNewTodo} />
-
-        {todoList.length > 0 ? (
-          <TodoData todoList={todoList} deleteATodo={deleteATodo} />
-        ) : (
-          <div className="todo-image">
-            <img src={reactLogo} alt="" className="logo" />
-          </div>
-        )}
-      </div>
 
       <Outlet />
       <Footer />
---
+++ src/components/todo/TodoApp.jsx
@@ -0,0 +1,46 @@
+import TodoNew from "./TodoNew";
+import TodoData from "./TodoData";
+import reactLogo from "../../assets/react.svg";
+import "./todo.css";
+
+import { useState } from "react";
+
+const TodoApp = () => {
+  const [todoList, setTodoList] = useState([]);
+
+  const addNewTodo = (name) => {
+    const newTodo = {
+      id: randomIntFromInterval(1, 1000000),
+      name: name,
+    };
+
+    setTodoList([...todoList, newTodo]);
+  };
+
+  const deleteATodo = (id) => {
+    const newTodoList = todoList.filter((item) => item.id !== id);
+    setTodoList(newTodoList);
+  };
+
+  const randomIntFromInterval = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  };
+
+  return (
+    <div className="todo-container">
+      <h1 className="todo-title">Todo List</h1>
+
+      <TodoNew addNewTodo={addNewTodo} />
+
+      {todoList.length > 0 ? (
+        <TodoData todoList={todoList} deleteATodo={deleteATodo} />
+      ) : (
+        <div className="todo-image">
+          <img src={reactLogo} alt="" className="logo" />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default TodoApp;
