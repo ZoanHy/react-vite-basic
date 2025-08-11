3af90ff9004769149b6f7173501bb5820c73bb62
ZoanHy <huyleezoan@gmail.com>
2025-08-11 13:35:28 +0700

    tạo header footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import TodoNew from "./components/todo/TodoNew";
 import TodoData from "./components/todo/TodoData";
-
 import reactLogo from "./assets/react.svg";
 import "./components/todo/todo.css";
 import { useState } from "react";
+import Header from "./components/layouts/header";
+import Footer from "./components/layouts/footer";
 
 function App() {
   const [todoList, setTodoList] = useState([]);
@@ -27,19 +28,23 @@ function App() {
   };
 
   return (
-    <div className="todo-container">
-      <h1 className="todo-title">Todo List</h1>
+    <>
+      <Header />
+      <div className="todo-container">
+        <h1 className="todo-title">Todo List</h1>
 
-      <TodoNew addNewTodo={addNewTodo} />
+        <TodoNew addNewTodo={addNewTodo} />
 
-      {todoList.length > 0 ? (
-        <TodoData todoList={todoList} deleteATodo={deleteATodo} />
-      ) : (
-        <div className="todo-image">
-          <img src={reactLogo} alt="" className="logo" />
-        </div>
-      )}
-    </div>
+        {todoList.length > 0 ? (
+          <TodoData todoList={todoList} deleteATodo={deleteATodo} />
+        ) : (
+          <div className="todo-image">
+            <img src={reactLogo} alt="" className="logo" />
+          </div>
+        )}
+      </div>
+      <Footer />
+    </>
   );
 }
 
