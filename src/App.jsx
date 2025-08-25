f3b46b0bc4e7826d0777445b648c00a9cc6c122f
Minh Huy Le Doan <huyleezoan@gmail.com>
2025-08-25 12:33:33 +0700

    Private route với react

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,10 @@ import { Outlet } from "react-router-dom";
 import { getAccountAPI } from "./services/api.service";
 import { useContext, useEffect } from "react";
 import { AuthContext } from "./components/context/auth.context";
+import { Spin } from "antd";
 
 function App() {
-  const { setUser } = useContext(AuthContext);
+  const { setUser, isAppLoading, setIsAppLoading } = useContext(AuthContext);
 
   useEffect(() => {
     fetchUserInfo();
@@ -21,19 +22,36 @@ function App() {
   };
   const fetchUserInfo = async () => {
     const res = await getAccountAPI();
-    await delay(3000);
+    // await delay(3000);
 
     if (res.data) {
       setUser(res.data.user);
-      console.log(">>> check user info", res.data);
+      // console.log(">>> check user info", res.data);
     }
+
+    setIsAppLoading(false);
   };
 
   return (
     <>
-      <Header />
-      <Outlet />
-      <Footer />
+      {isAppLoading ? (
+        <div
+          style={{
+            position: "fixed",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+          }}
+        >
+          <Spin />
+        </div>
+      ) : (
+        <>
+          <Header />
+          <Outlet />
+          <Footer />
+        </>
+      )}
     </>
   );
 }
